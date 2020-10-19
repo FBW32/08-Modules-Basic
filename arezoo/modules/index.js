fce6d241f755570583209ec2dcf7da9606e7ed39
arezoofaraji <arezoo.faraji66@gmail.com>
2020-10-19 12:00:22 +0200

    08 done

---
+++ arezoo/modules/index.js
@@ -0,0 +1,7 @@
+import clickImage from "./click.js";
+import hoverheader from "./hover.js";
+import scrollwindow from "./scroll.js";
+
+console.log(clickImage);
+console.log(scrollwindow);
+console.log(hoverheader);
