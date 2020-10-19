dfa9c79e04a029e62d530f574f354cf73d860d0a
Irina <irinagmarques@hotmail.com>
2020-10-19 13:24:29 +0200

    solution

---
+++ irina/modules/index.js
@@ -0,0 +1,10 @@
+const { import, import } = require("mathjs");
+
+
+import {clickImages} from './click.js';
+import {hoverHeader} from './hover.js';
+import { scrollWindow } from './scroll.js'
+
+clickImages();
+hoverHeader();
+scrollWindow();
